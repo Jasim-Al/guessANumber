2a1711bfdd577ea31dd03da4a217a8be85ced9a0
Jasim-Al <dev.mejasim@gmail.com>
2021-11-16 21:22:37 +0530

    INITIAL COMMIT

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
